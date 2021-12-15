f7d1f26d2b46807b70ed4b0a72ac8d28c41f184b
rgled002 <rgled002@gold.ac.uk>
2021-12-15 19:16:47 +0000

    Default Line props

--- engine/Line.ts
+++ engine/Line.ts
@@ -1,5 +1,5 @@
-import Engine from './Engine.js';
 import GameObject from './GameObject.js';
+import Vector2 from './Vector2.js';
 
 interface LineProperties {
   tag: string;
@@ -8,6 +8,14 @@ interface LineProperties {
   p2: Vector2;
   zIndex: string;
 }
+
+const defaultProps = {
+  tag: 'line',
+  width: 1,
+  zIndex: '0',
+  p1: new Vector2(),
+  p2: new Vector2(),
+};
 
 export default class Line extends GameObject {
   tag: string;
@@ -18,17 +26,20 @@ export default class Line extends GameObject {
   y2: number;
   zIndex: string;
 
-  constructor(props: LineProperties = {
-    tag: 'line', width: 1, zIndex: '0', p1: { x: 0, y: 0 }, p2: { x: 0, y: 0 }
-  }) {
-    super(props.tag, props.zIndex);
-    this.width = props.width;
-    this.x1 = props.p1.x;
-    this.y1 = props.p1.y;
-    this.x2 = props.p2.x;
-    this.y2 = props.p2.y;
+  constructor(props: LineProperties) {
+    super(props.tag || defaultProps.tag, props.zIndex || defaultProps.zIndex);
 
-    
+    const defaultedProps = {
+      ...defaultProps,
+      ...props,
+    };
+
+    this.width = defaultedProps.width;
+    this.x1 = defaultedProps.p1.x;
+    this.y1 = defaultedProps.p1.y;
+    this.x2 = defaultedProps.p2.x;
+    this.y2 = defaultedProps.p2.y;
+
     this.registerSelf();
   }
 }
