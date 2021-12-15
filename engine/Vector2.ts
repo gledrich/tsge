1c0c666dd75c3796f741191e01132f9872daf434
rgled002 <rgled002@gold.ac.uk>
2021-12-15 19:16:59 +0000

    Vector2 Class

--- engine/Vector2.ts
+++ engine/Vector2.ts
@@ -2,3 +2,13 @@ interface Vector2 {
   x: number;
   y: number;
 }
+
+export default class implements Vector2 {
+  x: number;
+  y: number;
+
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
