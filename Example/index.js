15043a1bb13f16267eb02e5c314d5855cde5e880
Rich <richgled25@gmail.com>
2026-04-04 19:43:24 +0100

    Implement Dino Survival demo game

--- Example/index.js
+++ Example/index.js
@@ -3,258 +3,214 @@ import Rectangle from '../built/Rectangle.js';
 import Vector2 from '../built/Vector2.js';
 import Text from '../built/Text.js';
 import Sprite from '../built/Sprite.js';
+import Scene from '../built/Scene.js';
 
 window.onload = () => {
-  new DemoGame();
+  new DinoSurvival();
 };
 
-class DemoGame {
-  constructor() {
-    window.showFPS = this.showFPS.bind(this);
-    window.hideFPS = this.hideFPS.bind(this);
-
-    // this.showFPS();
-
-    // 0: Load, 1: Play, 2: Restart
-    this.gameState = 0;
-
-    this.game = new Engine(
-      {
-        onLoad: this.onLoad.bind(this),
-        update: this.update.bind(this),
-      },
-      {
-        title: 'Demo Game',
-        backgroundColour: '#A7DCCC',
-      },
-    );
-
-    this.game.cursor = 'pointer';
-
-    this.game.callbacks.onLoad();
-
-    this.completedText = new Text({
-      tag: 'completedText',
-      colour: 'white',
-      backgroundColour: '#43aa8b',
-      fontSize: 50,
-      zIndex: 10,
-      text: 'Completed',
-      height: 300,
-      width: 500,
-      register: false,
-    });
-    this.completedText.position = new Vector2(
-      window.innerWidth / 2 - this.completedText.width / 2,
-      window.innerHeight / 2 - this.completedText.height / 2,
-    );
-
-    this.restartText = new Text({
-      tag: 'restartText',
-      colour: 'white',
-      backgroundColour: '#577590',
-      fontSize: 30,
-      zIndex: 20,
-      text: 'Restart',
-      register: false,
-      onClick: () => {
-        this.restart();
-      },
-    });
-    this.restartText.position = new Vector2(
-      this.completedText.position.x
-        + (this.completedText.width / 2 - this.restartText.width / 2),
-      this.completedText.position.y
-        + (this.completedText.height - this.restartText.height),
-    );
+class MenuScene extends Scene {
+  constructor(game, onStart) {
+    super();
+    this.game = game;
+    this.onStart = onStart;
   }
 
   onLoad() {
-    console.log('loaded game');
+    this.game.cursor = 'pointer';
 
-    this.food = new Set();
+    new Text({
+      tag: 'title',
+      text: 'Dino Survival',
+      fontSize: 60,
+      colour: 'white',
+      position: new Vector2(window.innerWidth / 2 - 250, 150),
+      width: 500,
+      zIndex: 10
+    });
 
-    if (this.gameState === 0) {
-      const titleText = new Text({
-        tag: 'titleText',
-        colour: 'white',
-        backgroundColour: '#43aa8b',
-        fontSize: 50,
-        zIndex: 10,
-        text: 'Demo Game',
-        height: 300,
-        width: 500,
-      });
-      titleText.position = new Vector2(
-        window.innerWidth / 2 - titleText.width / 2,
-        window.innerHeight / 2 - titleText.height / 2,
-      );
-
-      const countDownText = new Text({
-        tag: 'restartText',
-        colour: 'white',
-        backgroundColour: '#577590',
-        width: 100,
-        fontSize: 30,
-        zIndex: 20,
-        text: '3',
-      });
-      countDownText.position = new Vector2(
-        titleText.position.x + (titleText.width / 2 - countDownText.width / 2),
-        titleText.position.y + (titleText.height - countDownText.height),
-      );
-
-      this.game.countdown(
-        3000,
-        () => {
-          countDownText.text = `${parseInt(countDownText.text, 10) - 1}`;
-        },
-        () => {
-          countDownText.destroySelf();
-        },
-      );
-
-      this.game
-        // remove start screen
-        .setTimeout(titleText.destroySelf.bind(titleText), 3000)
-        .then(() => {
-          this.gameState = 1;
-
-          for (let i = 0; i < 10; i += 1) {
-            const dinoImg = new Image();
-            dinoImg.src = './sprites/DinoSprites - doux.png';
-            const food = new Sprite({
-              img: dinoImg,
-              rows: 1,
-              cols: 24,
-              position: new Vector2(
-                Math.floor(Math.random() * (window.innerWidth - 300)),
-                Math.floor(i * (window.innerHeight / 10)),
-              ),
-              startCol: 20,
-              endCol: 24,
-              tag: 'dino',
-            });
-
-            food.flipLeft = [true, false][(Math.random() * 2) - 1];
-            food.flipRight = !!food.flipLeft;
-
-            this.food.add(food);
-          }
-
-          // draw player
-          this.player = new Rectangle({
-            position: new Vector2(
-              window.innerWidth / 2,
-              window.innerHeight / 2,
-            ),
-            width: 50,
-            height: 50,
-            colour: '#F94144',
-            zIndex: 1,
-          });
-          this.player.grow = () => {
-            const growBy = 10;
-
-            this.player.width += growBy;
-            this.player.height += growBy;
-          };
-        });
-    }
-  }
-
-  update() {
-    if (this.fpsCounter) {
-      this.fpsCounter.text = `FPS: ${this.game.fps}`;
-    }
-
-    // Check if game is playing
-    if (this.gameState === 1) {
-      // hide mouse cursor
-      this.game.cursor = 'none';
-
-      // center player around mouse pos
-      this.player.position = new Vector2(
-        this.game.mouseX - this.player.width / 2,
-        this.game.mouseY - this.player.height / 2,
-      );
-
-      // handle food + player collision
-      this.food.forEach((pieceOfFood, i) => {
-        setTimeout(() => pieceOfFood.play(), Math.random() * 1);
-
-        const speed = 3;
-
-        if (pieceOfFood.flip) {
-          pieceOfFood.position.x -= speed;
-        } else {
-          pieceOfFood.position.x += speed;
-        }
-
-        if (
-          pieceOfFood.position.x >= window.innerWidth - pieceOfFood.frameWidth
-        ) {
-          pieceOfFood.flip = true;
-        }
-
-        if (
-          pieceOfFood.position.x <= 0
-        ) {
-          pieceOfFood.flip = false;
-        }
-
-        if (this.player.hasCollided(pieceOfFood)) {
-          if (this.food.size === 1) {
-            pieceOfFood.stop();
-          } else {
-            setTimeout(() => {
-              pieceOfFood.stop();
-            }, 300);
-          }
-
-          this.food.delete(pieceOfFood);
-          this.player.grow();
-        }
-      });
-
-      if (!this.food.size) {
-        this.gameState = 2;
-      }
-    }
-
-    // game ended
-    if (!this.food.size && this.gameState === 2) {
-      this.player.destroySelf();
-      this.game.cursor = 'pointer';
-
-      if (!this.completedText.registered) {
-        this.completedText.registerSelf();
-      }
-
-      if (!this.restartText.registered) {
-        this.restartText.registerSelf();
-      }
-    }
-  }
-
-  restart() {
-    this.gameState = 0;
-    this.completedText.destroySelf();
-    this.restartText.destroySelf();
-    this.player.destroySelf();
-    this.onLoad();
-  }
-
-  showFPS() {
-    this.fpsCounter = new Text({
-      tag: 'fpsCounterText',
-      colour: '#577590',
+    new Text({
+      tag: 'startBtn',
+      text: 'Click to Start',
+      fontSize: 30,
+      colour: '#43aa8b',
+      backgroundColour: 'white',
+      position: new Vector2(window.innerWidth / 2 - 100, 350),
+      width: 200,
+      height: 60,
       zIndex: 10,
-      text: 'FPS: 0',
+      onClick: () => this.onStart()
+    });
+  }
+}
+
+class PlayScene extends Scene {
+  constructor(game, dinoImg, onGameOver) {
+    super();
+    this.game = game;
+    this.dinoImg = dinoImg;
+    this.onGameOver = onGameOver;
+    this.stars = [];
+    this.meteors = [];
+    this.startTime = Date.now();
+    this.score = 0;
+  }
+
+  onLoad() {
+    this.game.cursor = 'none';
+
+    // Drifting Stars
+    for (let i = 0; i < 80; i++) {
+      const star = new Rectangle({
+        position: new Vector2(Math.random() * window.innerWidth, Math.random() * window.innerHeight),
+        width: 2,
+        height: 2,
+        colour: 'rgba(255, 255, 255, 0.4)',
+        zIndex: 0
+      });
+      star.speed = 1 + Math.random() * 2;
+      this.stars.push(star);
+    }
+
+    this.player = new Sprite({
+      img: this.dinoImg,
+      rows: 1,
+      cols: 24,
+      position: new Vector2(window.innerWidth / 2, window.innerHeight / 2),
+      startCol: 4,
+      endCol: 10,
+      tag: 'player',
+      zIndex: 5
+    });
+    this.player.play();
+
+    this.scoreText = new Text({
+      tag: 'score',
+      text: 'Time: 0s',
+      fontSize: 24,
+      colour: 'white',
+      position: new Vector2(20, 20),
+      width: 150,
+      zIndex: 10
     });
   }
 
-  hideFPS() {
-    this.fpsCounter.destroySelf();
+  update() {
+    // Player follow mouse
+    const targetX = this.game.mouseX - (this.player.frameWidth * 3) / 2;
+    const targetY = this.game.mouseY - (this.player.frameHeight * 3) / 2;
+    this.player.position.x += (targetX - this.player.position.x) * 0.2;
+    this.player.position.y += (targetY - this.player.position.y) * 0.2;
+
+    // Flip dino based on mouse
+    this.player.flip = this.game.mouseX < this.player.position.x + (this.player.frameWidth * 3) / 2;
+
+    // Star drift
+    this.stars.forEach(star => {
+      star.position.y += star.speed;
+      if (star.position.y > window.innerHeight) star.position.y = -5;
+    });
+
+    // Spawn Meteors
+    if (Math.random() < 0.05) {
+      const meteor = new Rectangle({
+        position: new Vector2(Math.random() * window.innerWidth, -50),
+        width: 30 + Math.random() * 40,
+        height: 30 + Math.random() * 40,
+        colour: '#F94144',
+        zIndex: 4
+      });
+      meteor.speed = 4 + Math.random() * 6;
+      this.meteors.push(meteor);
+    }
+
+    // Update Meteors & Collision
+    this.meteors = this.meteors.filter(meteor => {
+      meteor.position.y += meteor.speed;
+      
+      if (this.player.hasCollided(meteor)) {
+        this.onGameOver(this.score);
+        return false;
+      }
+
+      if (meteor.position.y > window.innerHeight + 100) {
+        meteor.destroySelf();
+        return false;
+      }
+      return true;
+    });
+
+    this.score = Math.floor((Date.now() - this.startTime) / 1000);
+    this.scoreText.text = `Time: ${this.score}s`;
+  }
+}
+
+class GameOverScene extends Scene {
+  constructor(game, score, onRestart) {
+    super();
+    this.game = game;
+    this.score = score;
+    this.onRestart = onRestart;
+  }
+
+  onLoad() {
+    this.game.cursor = 'pointer';
+
+    new Text({
+      tag: 'gameOver',
+      text: 'GAME OVER',
+      fontSize: 60,
+      colour: '#F94144',
+      position: new Vector2(window.innerWidth / 2 - 200, 150),
+      width: 400,
+      zIndex: 10
+    });
+
+    new Text({
+      tag: 'finalScore',
+      text: `You survived ${this.score} seconds!`,
+      fontSize: 30,
+      colour: 'white',
+      position: new Vector2(window.innerWidth / 2 - 200, 250),
+      width: 400,
+      zIndex: 10
+    });
+
+    new Text({
+      tag: 'restartBtn',
+      text: 'Try Again',
+      fontSize: 25,
+      colour: 'white',
+      backgroundColour: '#577590',
+      position: new Vector2(window.innerWidth / 2 - 100, 400),
+      width: 200,
+      height: 50,
+      zIndex: 10,
+      onClick: () => this.onRestart()
+    });
+  }
+}
+
+class DinoSurvival {
+  constructor() {
+    this.dinoImg = new Image();
+    this.dinoImg.src = './sprites/DinoSprites - doux.png';
+
+    this.game = new Engine(
+      { onLoad: () => this.showMenu(), update: () => {} },
+      { title: 'Dino Survival', backgroundColour: '#264653' }
+    );
+  }
+
+  showMenu() {
+    Engine.currentScene = new MenuScene(this.game, () => this.startGame());
+  }
+
+  startGame() {
+    Engine.currentScene = new PlayScene(this.game, this.dinoImg, (score) => this.showGameOver(score));
+  }
+
+  showGameOver(score) {
+    Engine.currentScene = new GameOverScene(this.game, score, () => this.showMenu());
   }
 }
