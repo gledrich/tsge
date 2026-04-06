d44d345c1def49e591e3939dd797e3f9c63136ce
Rich <richgled25@gmail.com>
2026-04-06 20:52:32 +0100

    Add tests for VisibilityComponent

---
+++ packages/dino-ge/src/VisibilityComponent.test.ts
@@ -0,0 +1,14 @@
+import VisibilityComponent from './VisibilityComponent';
+
+describe('VisibilityComponent', () => {
+  it('initialises with default value', () => {
+    const component = new VisibilityComponent();
+    expect(component.visible).toBe(true);
+  });
+
+  it('allows updating visible state', () => {
+    const component = new VisibilityComponent();
+    component.visible = false;
+    expect(component.visible).toBe(false);
+  });
+});
