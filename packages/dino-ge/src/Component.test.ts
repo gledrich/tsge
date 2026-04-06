2ce4134751f60ad4d8663a242c960f5189689739
Rich <richgled25@gmail.com>
2026-04-06 20:52:53 +0100

    Add tests for Component

---
+++ packages/dino-ge/src/Component.test.ts
@@ -0,0 +1,16 @@
+import Component from './Component';
+import GameObject from './GameObject';
+
+// Concrete implementation for testing
+class MockComponent extends Component {}
+class MockGameObject extends GameObject {}
+
+describe('Component', () => {
+  it('allows setting and getting gameObject', () => {
+    const component = new MockComponent();
+    const gameObject = new MockGameObject('test', 0);
+    
+    component.gameObject = gameObject;
+    expect(component.gameObject).toBe(gameObject);
+  });
+});
